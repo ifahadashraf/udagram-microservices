a20597002d78139828940ebb262c064dee3aa070
Fahad Ashraf <fahadashraf9612@gmail.com>
2021-09-08 07:46:34 +0500

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=database-1.ckb5zlpnq30f.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=bucket-fahad-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=fahad
+export JWT_SECRET=hello
+export URL=http://localhost:8100
