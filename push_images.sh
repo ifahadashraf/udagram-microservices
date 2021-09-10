90ccff0998521f4c2fce7268a1c30adaaa3e6d85
Fahad Ashraf <fahadashraf9612@gmail.com>
2021-09-10 19:05:34 +0500

    Final commit

---
+++ push_images.sh
@@ -0,0 +1,11 @@
+# udactiy-restapi-feed image
+docker push ifahadashraf/udagram-restapi-feed:latest
+
+# udactiy-restapi-user image
+docker push ifahadashraf/udagram-restapi-user:latest
+
+# udacity-frontend image
+docker push ifahadashraf/udagram-frontend:latest
+
+# reverseproxy image
+docker push ifahadashraf/reverseproxy:latest
